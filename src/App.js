a59519b2d9e4e7a48360de3dd97193e1439cd7dc
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-02-13 11:03:50 +0100

    Uploading to github

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const App = () => {
+
+  const Title = styled.h1 `
+    font-size: 50px;
+    text-align: center;
+    color: salmon;
+  `;
+
+  return (
+    <div>
+      <Title>
+        <h1>HOLA MUNDO</h1>
+      </Title>
+    </div>
+  );
+}
+
+export default App;
